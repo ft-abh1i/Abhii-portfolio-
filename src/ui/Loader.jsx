505991b0d5ae05a503c8875ff3f38398be67c651
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-05-29 16:10:51 +0530

    Add welcome state to loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react';
 
 export function Loader() {
   const [isDone, setIsDone] = useState(false);
+  const [showWelcome, setShowWelcome] = useState(false);
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    const duration = 3000;
+    const duration = 4000;
     const startedAt = Date.now();
 
     const interval = window.setInterval(() => {
@@ -15,7 +16,8 @@ export function Loader() {
 
       if (nextProgress >= 100) {
         window.clearInterval(interval);
-        window.setTimeout(() => setIsDone(true), 260);
+        setShowWelcome(true);
+        window.setTimeout(() => setIsDone(true), 1800);
       }
     }, 30);
 
@@ -23,32 +25,38 @@ export function Loader() {
   }, []);
 
   return (
-    <div className={`loader ${isDone ? 'loader-hidden' : ''}`}>
-      <div className="loader-marquee loader-marquee-top">
-        <div className="loader-marquee-track loader-left-to-right">
-          <span>Full Stack Developer</span>
-          <span>Full Stack Developer</span>
-          <span>Full Stack Developer</span>
-          <span>Full Stack Developer</span>
-        </div>
-      </div>
+    <div className={`loader ${isDone ? 'loader-hidden' : ''} ${showWelcome ? 'loader-welcome-active' : ''}`}>
+      {!showWelcome && (
+        <>
+          <div className="loader-marquee loader-marquee-top">
+            <div className="loader-marquee-track loader-left-to-right">
+              <span>Full Stack Developer</span>
+              <span>Full Stack Developer</span>
+              <span>Full Stack Developer</span>
+              <span>Full Stack Developer</span>
+            </div>
+          </div>
 
-      <div className="loader-center">
-        <div className="loader-count">{progress}%</div>
-        <div className="loader-bar">
-          <span style={{ width: `${progress}%` }} />
-        </div>
-        <div className="loader-caption">Loading</div>
-      </div>
+          <div className="loader-center">
+            <div className="loader-count">{progress}%</div>
+            <div className="loader-bar">
+              <span style={{ width: `${progress}%` }} />
+            </div>
+            <div className="loader-caption">Loading</div>
+          </div>
 
-      <div className="loader-marquee loader-marquee-bottom">
-        <div className="loader-marquee-track loader-right-to-left">
-          <span>Creative Web Designer</span>
-          <span>Creative Web Designer</span>
-          <span>Creative Web Designer</span>
-          <span>Creative Web Designer</span>
-        </div>
-      </div>
+          <div className="loader-marquee loader-marquee-bottom">
+            <div className="loader-marquee-track loader-right-to-left">
+              <span>Creative Web Designer</span>
+              <span>Creative Web Designer</span>
+              <span>Creative Web Designer</span>
+              <span>Creative Web Designer</span>
+            </div>
+          </div>
+        </>
+      )}
+
+      {showWelcome && <div className="loader-welcome">Welcome</div>}
     </div>
   );
 }
