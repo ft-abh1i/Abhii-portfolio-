144cdc51ff983abe92aad12e923f0b1e4405a18e
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-05-29 16:17:32 +0530

    Show welcome inside loader counter

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -16,8 +16,10 @@ export function Loader() {
 
       if (nextProgress >= 100) {
         window.clearInterval(interval);
-        setShowWelcome(true);
-        window.setTimeout(() => setIsDone(true), 1800);
+        window.setTimeout(() => {
+          setShowWelcome(true);
+          window.setTimeout(() => setIsDone(true), 1800);
+        }, 400);
       }
     }, 30);
 
@@ -26,37 +28,31 @@ export function Loader() {
 
   return (
     <div className={`loader ${isDone ? 'loader-hidden' : ''} ${showWelcome ? 'loader-welcome-active' : ''}`}>
-      {!showWelcome && (
-        <>
-          <div className="loader-marquee loader-marquee-top">
-            <div className="loader-marquee-track loader-left-to-right">
-              <span>Full Stack Developer</span>
-              <span>Full Stack Developer</span>
-              <span>Full Stack Developer</span>
-              <span>Full Stack Developer</span>
-            </div>
-          </div>
+      <div className="loader-marquee loader-marquee-top">
+        <div className="loader-marquee-track loader-left-to-right">
+          <span>Full Stack Developer</span>
+          <span>Full Stack Developer</span>
+          <span>Full Stack Developer</span>
+          <span>Full Stack Developer</span>
+        </div>
+      </div>
 
-          <div className="loader-center">
-            <div className="loader-count">{progress}%</div>
-            <div className="loader-bar">
-              <span style={{ width: `${progress}%` }} />
-            </div>
-            <div className="loader-caption">Loading</div>
-          </div>
+      <div className="loader-center">
+        <div className="loader-count">{showWelcome ? 'Welcome' : `${progress}%`}</div>
+        <div className="loader-bar">
+          <span style={{ width: `${progress}%` }} />
+        </div>
+        <div className="loader-caption">{showWelcome ? 'Entering' : 'Loading'}</div>
+      </div>
 
-          <div className="loader-marquee loader-marquee-bottom">
-            <div className="loader-marquee-track loader-right-to-left">
-              <span>Creative Web Designer</span>
-              <span>Creative Web Designer</span>
-              <span>Creative Web Designer</span>
-              <span>Creative Web Designer</span>
-            </div>
-          </div>
-        </>
-      )}
-
-      {showWelcome && <div className="loader-welcome">Welcome</div>}
+      <div className="loader-marquee loader-marquee-bottom">
+        <div className="loader-marquee-track loader-right-to-left">
+          <span>Creative Web Designer</span>
+          <span>Creative Web Designer</span>
+          <span>Creative Web Designer</span>
+          <span>Creative Web Designer</span>
+        </div>
+      </div>
     </div>
   );
 }
