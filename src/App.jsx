a5020315da4f4889093d7b138b1cbae658cc5854
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-05-29 15:24:23 +0530

    Remove unfinished cursor import

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import { Services } from './sections/services/Services.jsx';
 import { Contact } from './sections/contact/Contact.jsx';
 import { Footer } from './sections/footer/Footer.jsx';
 import { Loader } from './ui/Loader.jsx';
-import { CursorGlow } from './ui/CursorGlow.jsx';
 
 export default function App() {
   return (
     <>
       <Loader />
-      <CursorGlow />
       <Header />
       <main>
         <Hero />
