b943ca84316988008745fc58dc1dcd29ffe1ce7c
abhijeetshrivastava132-sudo <abhijeetshrivastava132@gmail.com>
2026-05-30 07:23:12 +0530

    Update loading screen text

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,27 +1,21 @@
 export default function Loader({ progress, phase }) {
   return (
-    <div className={`loader ${phase === 'welcome' ? 'loader-welcome-active' : ''} ${phase === 'hidden' ? 'loader-hidden' : ''}`}>
-      {phase === 'welcome' ? (
-        <div className="loader-welcome">Welcome</div>
-      ) : (
-        <>
-          <div className="loader-marquee loader-marquee-top" aria-hidden="true">
-            <div className="loader-marquee-track loader-left-to-right">
-              <span>ABHII DESIGNS</span><span>WEB DEVELOPER</span><span>PORTFOLIO</span><span>ABHII DESIGNS</span><span>WEB DEVELOPER</span><span>PORTFOLIO</span>
-            </div>
-          </div>
-          <div className="loader-center">
-            <div className="loader-count">{String(progress).padStart(2, '0')}</div>
-            <div className="loader-bar"><span style={{ width: `${progress}%` }} /></div>
-            <div className="loader-caption">Loading Portfolio</div>
-          </div>
-          <div className="loader-marquee loader-marquee-bottom" aria-hidden="true">
-            <div className="loader-marquee-track loader-right-to-left">
-              <span>DESIGN</span><span>BUILD</span><span>LAUNCH</span><span>DESIGN</span><span>BUILD</span><span>LAUNCH</span><span>DESIGN</span><span>BUILD</span><span>LAUNCH</span>
-            </div>
-          </div>
-        </>
-      )}
+    <div className={`loader ${phase === 'hidden' ? 'loader-hidden' : ''}`}>
+      <div className="loader-marquee loader-marquee-top" aria-hidden="true">
+        <div className="loader-marquee-track loader-left-to-right">
+          <span>FULL STACK DEVELOPER</span><span>FULL STACK DEVELOPER</span><span>FULL STACK DEVELOPER</span><span>FULL STACK DEVELOPER</span>
+        </div>
+      </div>
+      <div className="loader-center">
+        <div className="loader-count">{String(progress).padStart(2, '0')}</div>
+        <div className="loader-bar"><span style={{ width: `${progress}%` }} /></div>
+        <div className="loader-caption">Preloading Portfolio</div>
+      </div>
+      <div className="loader-marquee loader-marquee-bottom" aria-hidden="true">
+        <div className="loader-marquee-track loader-right-to-left">
+          <span>CREATIVE WEB DESIGNER</span><span>CREATIVE WEB DESIGNER</span><span>CREATIVE WEB DESIGNER</span><span>CREATIVE WEB DESIGNER</span>
+        </div>
+      </div>
     </div>
   );
 }
