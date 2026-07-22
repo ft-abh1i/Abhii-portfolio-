29ab184a14bc70d0001f51f842c488912c40e93d
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-07-22 11:30:54 +0530

    Restore original portfolio homepage entry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Portfolio from "@/components/Portfolio";
-import ReferenceHero from "@/components/ReferenceHero";
 
 export default function Home() {
-  return (
-    <>
-      <ReferenceHero />
-      <Portfolio />
-    </>
-  );
+  return <Portfolio />;
 }
